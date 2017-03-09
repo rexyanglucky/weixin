798cdd269c2268fb9c04f655153336fa57dc1643
yangjin <yangjin@mofangge.com>
2017-03-09 11:48:22 +0800

    提交

--- egword/gulp/task/default.js
+++
@@ -1,4 +0,0 @@
-var gulp=require('gulp');
-var gulpsync=require('gulp-sync')(gulp);
-gulp.task('default',gulpsync.sync(['clean',['less','images','js','webpack'],'watch']));
-gulp.task('package', gulpsync.sync(['clean', ['minless', 'minimage', 'js', 'minjs']]));
